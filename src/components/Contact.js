8fb34a7de43af02cfed688a923989540adaf59a6
Sachin Verma <sachinverma53121@gmail.com>
2021-01-19 18:41:51 +0530

    subscribe fetaure

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,9 +1,39 @@
-import React from 'react'
-import styled from 'styled-components'
+import React from "react";
+import styled from "styled-components";
 
 const Contact = () => {
-  return <h4>contact section</h4>
-}
+  return (
+    <Wrapper>
+      <div className="section-center">
+        <h3>Join out newsletter and get 25% off</h3>
+        <div className="content">
+          <p>
+            {" "}
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione
+            necessitatibus ex, aliquam et veritatis placeat totam debitis rem,
+            blanditiis harum, esse laborum impedit mollitia fuga velit maiores
+            voluptatibus sunt dicta.
+          </p>
+          <form
+            method="POST"
+            action="https://formspree.io/f/xbjpwdyq"
+            className="contact-form"
+          >
+            <input
+              type="email"
+              name="_replyto"
+              placeholder="enter email"
+              className="form-input"
+            />
+            <button type="submit" className="submit-btn">
+              subscribe
+            </button>
+          </form>
+        </div>
+      </div>
+    </Wrapper>
+  );
+};
 const Wrapper = styled.section`
   padding: 5rem 0;
   h3 {
@@ -67,6 +97,6 @@ const Wrapper = styled.section`
   @media (min-width: 1280px) {
     padding: 15rem 0;
   }
-`
+`;
 
-export default Contact
+export default Contact;
