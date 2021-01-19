6986f1830483d44622d5d183703b86ceef169baa
Sachin Verma <sachinverma53121@gmail.com>
2021-01-19 22:04:06 +0530

    single product add to cart colors

--- src/components/AddToCart.js
+++ src/components/AddToCart.js
@@ -1,13 +1,38 @@
-import React, { useState } from 'react'
-import styled from 'styled-components'
-import { Link } from 'react-router-dom'
-import { FaCheck } from 'react-icons/fa'
-import { useCartContext } from '../context/cart_context'
-import AmountButtons from './AmountButtons'
+import React, { useState } from "react";
+import styled from "styled-components";
+import { Link } from "react-router-dom";
+import { FaCheck } from "react-icons/fa";
 
-const AddToCart = () => {
-  return <h4>addToCart </h4>
-}
+import { useCartContext } from "../context/cart_context";
+import AmountButtons from "./AmountButtons";
+
+const AddToCart = ({ product }) => {
+  const { id, stock, colors } = product;
+  const [mainColor, setMainColor] = useState(colors[0]);
+
+  return (
+    <Wrapper>
+      <div className="colors">
+        <span>colors :</span>
+        <div>
+          {colors.map((color, index) => {
+            return (
+              <button
+                key={index}
+                className={`color-btn ${mainColor === color ? "active" : null}`}
+                style={{ background: color }}
+                onClick={() => setMainColor(color)}
+              >
+                {mainColor === color ? <FaCheck /> : null}
+              </button>
+            );
+          })}
+        </div>
+      </div>
+      <div className="btn-container"></div>
+    </Wrapper>
+  );
+};
 
 const Wrapper = styled.section`
   margin-top: 2rem;
@@ -53,5 +78,5 @@ const Wrapper = styled.section`
     margin-top: 1rem;
     width: 140px;
   }
-`
-export default AddToCart
+`;
+export default AddToCart;
--- src/pages/SingleProductPage.js
+++ src/pages/SingleProductPage.js
@@ -85,7 +85,7 @@ const SingleProductPage = () => {
               {company}
             </p>
             <hr />
-            {stock > 0 && <AddToCart />}
+            {stock > 0 && <AddToCart product={product} />}
           </section>
         </div>
       </div>
