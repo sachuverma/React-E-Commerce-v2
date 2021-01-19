e697717a0d5583f2c54055a2b04b92418df279cb
Sachin Verma <sachinverma53121@gmail.com>
2021-01-19 16:35:20 +0530

    router navbar setup

--- src/App.js
+++ src/App.js
@@ -1,9 +1,55 @@
-import React from 'react'
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
-import { Navbar, Sidebar, Footer } from './components'
+import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import styled from "styled-components";
+
+import { Navbar, Sidebar, Footer } from "./components";
+import {
+  Home,
+  SingleProduct,
+  Cart,
+  Checkout,
+  Error,
+  About,
+  Products,
+  PrivateRoute,
+} from "./pages";
 
 function App() {
-  return <h4>comfy sloth starter</h4>
+  return (
+    <Router>
+      <Navbar />
+      <Sidebar />
+
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+
+        <Route exact path="/about">
+          <About />
+        </Route>
+
+        <Route exact path="/cart">
+          <Cart />
+        </Route>
+
+        <Route exact path="/products">
+          <Products />
+        </Route>
+
+        <Route exact path="/products/:id" children={<SingleProduct />} />
+
+        <Route exact path="/checkout">
+          <Checkout />
+        </Route>
+
+        <Route path="*">
+          <Error />
+        </Route>
+      </Switch>
+      <Footer />
+    </Router>
+  );
 }
 
-export default App
+export default App;
