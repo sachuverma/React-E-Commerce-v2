e99d9a15b030bbb5e852b1a4750ebc6fc1f47684
Sachin Verma <sachinverma53121@gmail.com>
2021-01-29 10:18:00 +0530

    loading height

--- src/components/Loading.js
+++ src/components/Loading.js
@@ -2,7 +2,10 @@ import React from "react";
 
 const Loading = () => {
   return (
-    <div className="section section-center" style={{ minHeight: "80vh" }}>
+    <div
+      className="section section-center"
+      style={{ minHeight: "calc(100vh - 80px)" }}
+    >
       <div className="loading"></div>
     </div>
   );
