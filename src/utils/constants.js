d1386cdb0c2dc44c987a07435c1aa784e7d2978e
Sachin Verma <sachinverma53121@gmail.com>
2021-02-23 23:31:04 +0530

    my api

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -42,6 +42,6 @@ export const services = [
   },
 ]
 
-export const products_url = 'https://course-api.com/react-store-products'
+export const products_url = 'https://sachu-ecommerce-api.herokuapp.com/api/products'
 
-export const single_product_url = `https://course-api.com/react-store-single-product?id=`
+export const single_product_url = `https://sachu-ecommerce-api.herokuapp.com/api/products/`
